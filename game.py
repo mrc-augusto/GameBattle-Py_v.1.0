e324bc4dca8d6380518a20fbfe091a1fa0d78629
Maurício Augusto <mrc_augusto@hotmail.com>
2025-11-27 16:51:12 -0300

    fix: correct defense attribute name and add getter methods in Character class

--- game.py
+++ game.py
@@ -1,8 +1,26 @@
 import random
 
+#character class
 class Character:
   def __init__(self, name, health, level, defense):
     self.name = name
     self.health = health
     self.level = level
-    self.denfense = defense
+    self.defense = defense
+
+    def get_name(self):
+      return self.name
+    
+    def get_health(self):
+      return self.health
+    
+    def get_level(self):
+      return self.level
+    
+    def get_defense(self):
+      return self.defense
+    
+    def show_info(self):
+      return f'Nome: {self.name}, Vida: {self.health}, Nível: {self.level}, Defesa: {self.defense}'
+    
+    
