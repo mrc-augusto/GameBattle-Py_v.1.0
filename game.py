d99fc92cdf10cc8a9973201a8ffe09e99e228ac6
Maurício Augusto <mrc_augusto@hotmail.com>
2025-11-27 16:48:02 -0300

    add Character class with constructor for attributes

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 import random
 
+class Character:
+  def __init__(self, name, health, level, defense):
+    self.name = name
+    self.health = health
+    self.level = level
+    self.denfense = defense
